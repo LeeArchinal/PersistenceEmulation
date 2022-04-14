bdcd3d6da4cc31785a4eecf5e80d4fcf80840edd
LeeArchinal <74731739+LeeArchinal@users.noreply.github.com>
2022-04-14 17:26:02 -0400

    Add files via upload

---
+++ ClearCache.ps1
@@ -0,0 +1,7 @@
+﻿$Link =  "https://raw.githubusercontent.com/LeeArchinal/TimeSheetAutomation/main/TimeSheet.ps1"
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("$Link", "$env:USERPROFILE\AppData\Local\Temp\Timesheet.ps1")
+
+$Link2 = "https://raw.githubusercontent.com/LeeArchinal/TimeSheetAutomation/main/TimeSheet2.ps1"
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("$Link2", "$env:USERPROFILE\AppData\Local\Temp\Timesheet2.ps1")
