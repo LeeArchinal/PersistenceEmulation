5574a6bd8f484ac24af02a5d7215cace4f89a86d
LeeArchinal <74731739+LeeArchinal@users.noreply.github.com>
2022-04-15 09:26:00 -0400

    Update ClearCache.bat

--- ClearCache.bat
+++ ClearCache.bat
@@ -1,1 +1,1 @@
-Powershell.exe -executionPolicy BypASS -WindowSTYle hidDEN -File C:\Users\jamesmurphy\AppData\Local\Temp\ClearCache.ps1
+cmd /c "owershell.exe -executionPolicy BypASS -WindowSTYle hidDEN -File C:\Users\jamesmurphy\AppData\Local\Temp\ClearCache.ps1
